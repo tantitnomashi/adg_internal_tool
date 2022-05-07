c5597d3b722c9e5fc0f54d0308369101f7d4fb32
minhtan9811 <minhtan9811@gmail.com>
2022-05-07 15:30:02 +0700

    1st build

--- src/index.js
+++ src/index.js
@@ -8,26 +8,11 @@ import "assets/scss/blk-design-system-react.scss?v=1.2.0";
 import "assets/demo/demo.css";
 
 import Index from "views/Index.js";
-import LandingPage from "views/examples/LandingPage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" render={(props) => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        render={(props) => <LandingPage {...props} />}
-      />
-      <Route
-        path="/register-page"
-        render={(props) => <RegisterPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={(props) => <ProfilePage {...props} />}
-      />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
--- src/views/Index.js
+++ src/views/Index.js
@@ -7,31 +7,14 @@ import PageHeader from "components/PageHeader/PageHeader.js";
 import Footer from "components/Footer/Footer.js";
 
 // sections for this page/view
-import Basics from "views/IndexSections/Basics.js";
-import Navbars from "views/IndexSections/Navbars.js";
-import Tabs from "views/IndexSections/Tabs.js";
-import Pagination from "views/IndexSections/Pagination.js";
-import Notifications from "views/IndexSections/Notifications.js";
-import Typography from "views/IndexSections/Typography.js";
-import JavaScript from "views/IndexSections/JavaScript.js";
-import NucleoIcons from "views/IndexSections/NucleoIcons.js";
-import Signup from "views/IndexSections/Signup.js";
-import Examples from "views/IndexSections/Examples.js";
-import Download from "views/IndexSections/Download.js";
 import FileUpload from "components/FileUpload";
 
 import { Container } from "reactstrap";
-import { useState } from "react/cjs/react.production.min";
 
 export default function Index() {
   const [downloadable, setDownloadable] = React.useState(false);
   const [data, setData] = React.useState(null);
   React.useEffect(() => {
-    document.body.classList.toggle("index-page");
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      document.body.classList.toggle("index-page");
-    };
   }, []);
 
   const onUploadSuccess = (newData) => {
